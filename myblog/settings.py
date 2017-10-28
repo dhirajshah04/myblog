3ad10faf0dba37bc70635990620c48f7dd22a847
dhirajshah04 <dhiraj.shah04@gmail.com>
2017-10-28 15:42:29 +0545

    heroku commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -137,5 +137,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+
+# Extra places for collectstatic to find static files.
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
